a6a0a56f145b2376d6e34bc21ac36f5584a5e2e0
Gary Tang <76119663+Gary0133@users.noreply.github.com>
2022-02-27 16:51:11 +0000

    Add files via upload

---
+++ Godish.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Godish : AITank
+{
+    public Dictionary<GameObject, float> targetTanksFound = new Dictionary<GameObject, float>();
+    public Dictionary<GameObject, float> consumablesFound = new Dictionary<GameObject, float>();
+    public Dictionary<GameObject, float> basesFound = new Dictionary<GameObject, float>();
+
+    public GameObject targetTankPosition;
+    public GameObject consumablePosition;
+    public GameObject basePosition;
+
+    /*******************************************************************************************************      
+    WARNING, do not include void Start(), use AITankStart() instead if you want to use Start method from Monobehaviour.
+    *******************************************************************************************************/
+    public override void AITankStart()
+    {
+
+    }
+
+    /*******************************************************************************************************       
+    WARNING, do not include void Update(), use AITankUpdate() instead if you want to use Update method from Monobehaviour.
+    *******************************************************************************************************/
+    public override void AITankUpdate()
+    {
+        
+    }
+
+    /*******************************************************************************************************       
+    WARNING, do not include void OnCollisionEnter(), use AIOnCollisionEnter() instead if you want to use Update method from Monobehaviour.
+    *******************************************************************************************************/
+    public override void AIOnCollisionEnter(Collision collision)
+    {
+    }
+}
