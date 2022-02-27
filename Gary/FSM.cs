3725e9203425564d40f22cd1252a5680d2761311
Gary Tang <76119663+Gary0133@users.noreply.github.com>
2022-02-27 17:34:02 +0000

    Add files via upload

---
+++ Gary/FSM.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM : Godish //Rename the inheritance if pushing to main and your script name is different
+{
+    public void Wander() 
+    {    
+    }
+};
